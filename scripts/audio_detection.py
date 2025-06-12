c833aa226f30331e214ced2648512ae18b3b4468
Ronnie Gertman <ronniegert2707@gmail.com>
2025-06-12 14:19:37 +0300

    added 4 octave support

--- scripts/audio_detection.py
+++ scripts/audio_detection.py
@@ -22,7 +22,7 @@ def audio_sampling():
         data = resample(data, num_samples_total)
 
     # Take the first NUM_SAMPLES samples
-    audio_samples = data[:NUM_SAMPLES]
+    audio_samples = data[NUM_SAMPLES*4:NUM_SAMPLES*5]
     time = [i / SAMPLING_RATE for i in range(len(audio_samples))]
     plt.figure(figsize=(10, 4))
     plt.plot(time, audio_samples)
@@ -50,7 +50,29 @@ def serial_send(time, audio_sample):
     print("Response from Arduino:", response)
     ser.close()
     
+def plot_fft(audio_sample):
+    # Perform FFT
+    fft_result = np.fft.fft(audio_sample)
+    freqs = np.fft.fftfreq(len(fft_result), 1/SAMPLING_RATE)
+
+    # Plot the FFT result
+    plt.figure(figsize=(10, 4))
+    plt.plot(freqs[:len(freqs)//2], np.abs(fft_result)[:len(fft_result)//2])
+    plt.title("FFT of Audio Samples")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Magnitude")
+    #mark the maximum frequency
+    plt.legend()
+    plt.xlim(0, SAMPLING_RATE / 2)
+    plt.grid()
+    plt.show()
+
+    #find core frequency real value signal
+    core_freq = freqs[np.argmax(np.abs(fft_result)[:len(fft_result)//2])]
+    print(f"Core frequency: {core_freq:.2f} Hz")
+
 if __name__ == "__main__":
     time, audio_sample = audio_sampling()
     serial_send(time, audio_sample)
+    # plot_fft(audio_sample)
 
